f6646afb88cce0ca80d4b82b9e8d1e8fca240bb5
nowratun oyshe <nowratunoyshe199807@gmail.com>
2022-08-29 05:29:00 +0600

    oyshe_last

--- registration.php
+++ registration.php
@@ -1,28 +1,43 @@
-
 <?php 
 
 include 'login_form.php';
 
-session_start();
-
 error_reporting(0);
 
+session_start();
+
 if (isset($_SESSION['username'])) {
-    header("Location: welcome.php");
+    header("Location: login.php");
 }
 
 if (isset($_POST['submit'])) {
+	$username = $_POST['username'];
 	$email = $_POST['email'];
 	$password = md5($_POST['password']);
+	$cpassword = md5($_POST['cpassword']);
 
-	$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
-	$result = mysqli_query($conn, $sql);
-	if ($result->num_rows > 0) {
-		$row = mysqli_fetch_assoc($result);
-		$_SESSION['username'] = $row['username'];
-		header("Location: welcome.php");
+	if ($password == $cpassword) {
+		$sql = "SELECT * FROM users WHERE email='$email'";
+		$result = mysqli_query($conn, $sql);
+		if (!$result->num_rows > 0) {
+			$sql = "INSERT INTO users (username, email, password)
+					VALUES ('$username', '$email', '$password')";
+			$result = mysqli_query($conn, $sql);
+			if ($result) {
+				echo "<script>alert('Wow! User Registration Completed.')</script>";
+				$username = "";
+				$email = "";
+				$_POST['password'] = "";
+				$_POST['cpassword'] = "";
+			} else {
+				echo "<script>alert('Woops! Something Wrong Went.')</script>";
+			}
+		} else {
+			echo "<script>alert('Woops! Email Already Exists.')</script>";
+		}
+		
 	} else {
-		echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
+		echo "<script>alert('Password Not Matched.')</script>";
 	}
 }
 
@@ -36,18 +51,15 @@ if (isset($_POST['submit'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>login</title>
+<title>Register</title>
 <!-- swiper css link  -->
 <link rel="stylesheet"  type="text/css"  href="style.css">
 <link rel="stylesheet"  type="text/css"
  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" />
 
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <link rel="stylesheet" type="text/css" href="style.css">
-
 
  <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -72,7 +84,7 @@ if (isset($_POST['submit'])) {
 
 <section class="header">
 
-   <a href="home.php" class="logo">MyEventPlanner.</a>
+   <a href="home.php" class="logo">tourbros.</a>
 
    <nav class="navbar">
       <a href="home.php">home</a>
@@ -97,17 +109,23 @@ if (isset($_POST['submit'])) {
 
 <div class="container">
 		<form action="" method="POST" class="login-email">
-			<p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
+            <p class="login-text" style="font-size: 2rem; font-weight: 800;">Register</p>
+			<div class="input-group">
+				<input type="text" placeholder="Username" name="username" value="<?php echo $username; ?>" required>
+			</div>
 			<div class="input-group">
 				<input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
 			</div>
 			<div class="input-group">
 				<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+            </div>
+            <div class="input-group">
+				<input type="password" placeholder="Confirm Password" name="cpassword" value="<?php echo $_POST['cpassword']; ?>" required>
 			</div>
 			<div class="input-group">
-				<button name="submit" class="btn">Login</button>
+				<button name="submit" class="btn">Register</button>
 			</div>
-			<p class="login-register-text">Don't have an account? <a href="registration.php">Register Here</a>.</p>
+			<p class="login-register-text">Have an account? <a href="login.php">Login Here</a>.</p>
 		</form>
 	</div>
 </sectiom>
