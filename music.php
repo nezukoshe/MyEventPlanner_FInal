2e785cc4675be50029d3faa8f3de26168c7f79c1
nowratun oyshe <nowratunoyshe199807@gmail.com>
2022-09-04 22:29:00 +0600

    oyshe_111

---
+++ music.php
@@ -0,0 +1,128 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+
+
+
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">My Event Planner.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+      <a href="login.php">login</a>
+      <a href="contactus.php">Contact Us</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/common.jpg)  no-repeat" width="600" height="300">
+   <h1>about us</h1>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+ 
+       <div class="box">
+          <h3>quick links</h3>
+          <a href="home.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Home</a>
+          <a href="about.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>About Us</a>
+          <a href="services.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Services</a>
+          <a href="gallery.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Gallery</a>
+          <a href="combo.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Combo</a>
+          <a href="reservation.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Reservation</a>
+       </div>
+ 
+       <div class="box">
+          <h3>extra links</h3>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-angle-right"></i> ask questions</a>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-angle-right"></i> about us</a>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-angle-right"></i> privacy policy</a>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-angle-right"></i> terms of use</a>
+       </div>
+ 
+       <div class="box">
+          <h3>contact info</h3>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+       </div>
+ 
+       <div class="box">
+          <h3>follow us</h3>
+          <a href="#" style="text-decoration: none"> <i class="fab fa-facebook-f"></i> facebook </a>
+          <a href="#" style="text-decoration: none"> <i class="fab fa-twitter"></i> twitter </a>
+          <a href="#" style="text-decoration: none"> <i class="fab fa-instagram"></i> instagram </a>
+          <a href="#" style="text-decoration: none"> <i class="fab fa-linkedin"></i> linkedin </a>
+       </div>
+ 
+    </div>
+ 
+    <div class="credit"> created by <span>Jaasia & Oyshe</span> | all rights reserved! </div>
+ 
+ </section>
+ 
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
--- registration.php
+++ registration.php
@@ -84,15 +84,15 @@ if (isset($_POST['submit'])) {
 
 <section class="header">
 
-   <a href="home.php" class="logo" style="text-decoration: none">MyEventPlanner.</a>
+   <a href="home.php" class="logo">tourbros.</a>
 
    <nav class="navbar">
-      <a href="home.php" style="text-decoration: none">home</a>
-      <a href="about.php" style="text-decoration: none">about</a>
-      <a href="package.php" style="text-decoration: none">package</a>
-      <a href="book.php" style="text-decoration: none">book</a>
-      <a href="login.php" style="text-decoration: none">login</a>
-      <a href="contactus.php" style="text-decoration: none">Contact Us</a>
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+      <a href="login.php">login</a>
+      <a href="contactus.php">Contact Us</a>
    </nav>
 
    <div id="menu-btn" class="fas fa-bars"></div>
@@ -125,7 +125,7 @@ if (isset($_POST['submit'])) {
 			<div class="input-group">
 				<button name="submit" class="btn">Register</button>
 			</div>
-			<p class="login-register-text">Have an account? <a href="login.php">Login Here</a>.</p>
+			<p class="login-register-text">Have an account? <a href="login.php" style="color: #6fdd97;">Login Here</a>.</p>
 		</form>
 	</div>
 </sectiom>
@@ -149,52 +149,54 @@ if (isset($_POST['submit'])) {
 
 
 
+<!--login  close-->
+
+
 <!-- footer section starts  -->
 
 <section class="footer">
 
-    <div class="box-container">
- 
-       <div class="box">
-          <h3>quick links</h3>
-          <a href="home.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Home</a>
-          <a href="about.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>About Us</a>
-          <a href="services.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Services</a>
-          <a href="gallery.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Gallery</a>
-          <a href="combo.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Combo</a>
-          <a href="reservation.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Reservation</a>
-       </div>
- 
-       <div class="box">
-          <h3>extra links</h3>
-          <a href="#"   style="text-decoration: none"> <i class="fas fa-angle-right"></i> ask questions</a>
-          <a href="#"   style="text-decoration: none"> <i class="fas fa-angle-right"></i> about us</a>
-          <a href="#"   style="text-decoration: none"> <i class="fas fa-angle-right"></i> privacy policy</a>
-          <a href="#"   style="text-decoration: none"> <i class="fas fa-angle-right"></i> terms of use</a>
-       </div>
- 
-       <div class="box">
-          <h3>contact info</h3>
-          <a href="#"   style="text-decoration: none"> <i class="fas fa-phone"></i> +123-456-7890 </a>
-          <a href="#"   style="text-decoration: none"> <i class="fas fa-phone"></i> +111-222-3333 </a>
-          <a href="#"   style="text-decoration: none"> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
-          <a href="#"   style="text-decoration: none"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
-       </div>
- 
-       <div class="box">
-          <h3>follow us</h3>
-          <a href="#" style="text-decoration: none"> <i class="fab fa-facebook-f"></i> facebook </a>
-          <a href="#" style="text-decoration: none"> <i class="fab fa-twitter"></i> twitter </a>
-          <a href="#" style="text-decoration: none"> <i class="fab fa-instagram"></i> instagram </a>
-          <a href="#" style="text-decoration: none"> <i class="fab fa-linkedin"></i> linkedin </a>
-       </div>
- 
-    </div>
- 
-    <div class="credit"> created by <span>Jaasia & Oyshe</span> | all rights reserved! </div>
- 
- </section>
- 
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> login</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +01733110068 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +01733110064 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> bony@gmail.com </a>
+         <a href="#"> <i class="fas fa-envelope"></i> raffin@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> mirpur, dhaka - 1216 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>AB & DM</span> | all rights reserved! </div>
+
+</section>
 
 <!-- footer section ends -->
 
