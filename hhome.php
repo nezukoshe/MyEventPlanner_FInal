3417d1cf8bdd3bc1ac2520160b57faf3796cba18
nowratun oyshe <nowratunoyshe199807@gmail.com>
2022-09-05 23:44:18 +0600

    oysheeeeeeeeeee

---
+++ hhome.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+  
+
+
+
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">My Event Planner.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+      <a href="login.php">login</a>
+      <a href="contactus.php">Contact Us</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home">
+
+<div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide" style="background:url(images/home1.jpg) no-repeat">
+            <div class="content">
+               <span>plan,participate,enjoy</span>
+               <h3>Give Surprises</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/home2.jpg) no-repeat">
+            <div class="content">
+               <span>plan,participate,enjoy</span>
+               <h3>Get the best taste</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/home3.jpg) no-repeat">
+            <div class="content">
+               <span>plan,participate,enjoy</span>
+               <h3>Enjoy the Beat</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+
+
+<!-- home about section starts  -->
+
+<section class="home-about">
+   
+   <div class="image">
+      <img src="images/about2.png"  alt="" width="400" height="450">
+   </div>
+
+   <div class="content">
+      
+      <p>" My Event Planner " is an website which contains all the information for planning a event , will help to choose your specific event , will estimate cost of the whole event. </p>
+      <a href="about.php" class="btn">know more</a>
+   </div>
+
+</section>
+
+<!-- home about section ends -->
+
+
+
+<!-- services section starts  -->
+
+<section class="services">
+
+   <h1 class="heading-title"> our services </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/decoration.jpg" alt="">
+         <h3><a href="decoration.php" class="btn">Decoration </a></h3>
+      </div>
+
+      <div class="box">
+         <img src="images/catering.jpg" alt="">
+         <h3><a href="decoration.php" class="btn">Catering</a></h3>
+      </div>
+
+      <div class="box">
+         <img src="images/photography.jpg" alt="">
+         <h3><a href="decoration.php" class="btn">Photography </a></h3>
+      </div>
+
+      <div class="box">
+         <img src="images/music.jpg" alt="">
+         <h3><a href="decoration.php" class="btn">Music </a></h3>
+      </div>
+
+      <div class="box">
+         <img src="images/invitation.jpg" alt="">
+         <h3><a href="decoration.php" class="btn">Invitation Card </a></h3>
+      </div>
+
+      <div class="box">
+         <img src="images/kids.jpg" alt="">
+         <h3><a href="decoration.php" class="btn">Kids Section </a></h3>
+      </div>
+
+      
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+
+<!-- home packages section starts  -->
+
+<section class="home-packages">
+
+   <h1 class="heading-title"> our packages </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/pack1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Wedding</h3>
+            <p>Wanna make your dream wedding plan? Why not? You'll not get the chance again!!!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/pack2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Birthday Party</h3>
+            <p>Why not a mega celebration of getting old !!! <br> Plan for your special one</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/pack3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Corporate Events</h3>
+            <p>Boss is angry? No worries. We are here to make your office event memorable </p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"> <a href="package.php" class="btn">load more</a> </div>
+
+</section>
+
+<!-- home packages section ends -->
+
+<!-- home offer section starts  -->
+
+<section class="home-offer">
+   <div class="content">
+      <h3>We are offering a discount on winter</h3>
+      <p>On the Working days enjoy 25% off on all of our services.Plan your event fast!!!</p>
+      <a href="book.php" class="btn">book now</a>
+   </div>
+</section>
+
+<!-- home offer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+ 
+       <div class="box">
+          <h3>quick links</h3>
+          <a href="home.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Home</a>
+          <a href="about.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>About Us</a>
+          <a href="services.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Services</a>
+          <a href="gallery.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Gallery</a>
+          <a href="combo.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Combo</a>
+          <a href="reservation.php"   style="text-decoration: none"><i class="fas fa-angle-right"></i>Reservation</a>
+       </div>
+ 
+       <div class="box">
+          <h3>extra links</h3>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-angle-right"></i> ask questions</a>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-angle-right"></i> about us</a>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-angle-right"></i> privacy policy</a>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-angle-right"></i> terms of use</a>
+       </div>
+ 
+       <div class="box">
+          <h3>contact info</h3>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
+          <a href="#"   style="text-decoration: none"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+       </div>
+ 
+       <div class="box">
+          <h3>follow us</h3>
+          <a href="#" style="text-decoration: none"> <i class="fab fa-facebook-f"></i> facebook </a>
+          <a href="#" style="text-decoration: none"> <i class="fab fa-twitter"></i> twitter </a>
+          <a href="#" style="text-decoration: none"> <i class="fab fa-instagram"></i> instagram </a>
+          <a href="#" style="text-decoration: none"> <i class="fab fa-linkedin"></i> linkedin </a>
+       </div>
+ 
+    </div>
+ 
+    <div class="credit"> created by <span>Jaasia & Oyshe</span> | all rights reserved! </div>
+ 
+ </section>
+ 
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
